77e6296c2f0496eaa11f619b7480ea4502d01022
Curtis Rueden <ctrueden@wisc.edu>
2020-06-19 18:29:26 -0500

    koi-turtle: remove commented out code

--- koi-turtle.py
+++ koi-turtle.py
@@ -5,10 +5,7 @@ path = 'gif/koi.gif'
 turtle.register_shape(path)
 
 turtle.shape(path)
-#t = turtle.Turtle(path)
 
 drawSVG('svg/cat.svg', 'black')
-#t.width(20)
-#t.forward(50)
 
 input('Press ENTER to continue')
